0fdec0d0ed67a1e07621579b1db914c39da62145
Trickshot1722 <tshot1722@gmail.com>
2023-08-19 13:26:37 +0300

    removed source file

--- heart_felt/heart_felt/settings.py
+++ heart_felt/heart_felt/settings.py
@@ -54,7 +54,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates/admin_dashboard/source')
+            os.path.join(BASE_DIR, 'templates/admin_dashboard/theme')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
